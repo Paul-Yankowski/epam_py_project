62f3d2e52e2dfc1e8d9c88096aeadb0f828453f6
Paul-Yankowski <p.yankowski.py@gmail.com>
2021-05-05 13:02:38 +0300

    database changed on sqlite

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,28 +1,29 @@
 from tkinter import *
 from tkinter.ttk import Treeview
 from bin import parser
-from postgre import db
+from sqlite import sqlite_db
 
 window = Tk()
 window.geometry('800x600')
 txt = Entry(window,width=20)
 txt.grid(column=1, row=0)
-
+tree = Treeview(window, selectmode='browse')
 
 
 def load_into_db():
     res = f'{txt.get()}'
-    db.write_db(res,res,parser.parse(res))
+    sqlite_db.write_db(res,res,parser.parse(res))
     #parser.parse(res)
 
 def load_from_db():
-    pass
+    for row in sqlite_db.read_db():
+        tree.insert("",'end',text=row)
+
 def wind():
     btn = Button(window, text="Загрузить в бд", command=load_into_db)
     btn.grid(column=2, row=0)
     btn1 = Button(window, text="Загрузить from бд", command=load_from_db)
     btn1.grid(column=2, row=0)
-    tree = Treeview(window, selectmode='browse')
     tree.place(x=100, y=200, width=550, height=200)
     scroll = Scrollbar(window, orient="vertical", command=tree.yview)
     scroll.place(x=650, y=200, height=200)
---
+++ sqlite/sqlite_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+connection = sqlite3.connect("epam_project.db")
+sql = connection.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS tags_data (
+    site_name text,
+    url text,
+    check_date text,
+    tags_info text)
+    """)
+
+def read_db():
+    sql.execute("select * from tags_data")
+    rec = sql.fetchall()
+    return rec
+
+def write_db (site,url,tags):
+    sql.execute('insert into tags_data(site_name,url,tags_info) '
+                   'values (\''+site+'\',\''+url+'\',\''+str(tags).replace('\'','\"',100000)+'\');')
+    connection.commit()
